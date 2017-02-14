4010547a1de49a4034b774ac7c179ab7ba88814c
Megan <megan.woodford93@gmail.com>
2017-02-14 00:08:22 +0000

    Successfully displaying list of avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,25 @@
 var request = require('request');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+  request.get(options, function (error, response, body) {
+    var data = JSON.parse(body);
+    if (!error && response.statusCode == 200) {
+      data.forEach(function(user) {
+        console.log(user.avatar_url);
+      })
+    }
+  })
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
 console.log('Welcome to the GitHub Avatar Downloader!');
