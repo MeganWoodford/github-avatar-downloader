317df4c180a8b9af92102ba4b596cdd12d9bf0e0
Megan <megan.woodford93@gmail.com>
2017-02-14 00:23:16 +0000

    Included request URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,9 @@
 var request = require('request');
+var GITHUB_USER = "MeganWoodford";
+var GITHUB_TOKEN = "185e83be2f65cebf2e6afd56ae7f0d1be08cbaaa";
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
@@ -23,3 +26,29 @@ getRepoContributors("jquery", "jquery", function(err, result) {
 });
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+/*
+------------------------------------------
+function getRepoContributors(repoOwner, repoName, cb) {
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+//dobbiamo definire option perche' ci serve un oggetto, quindi definiamo i parametri
+var option = {url : requestURL, headers: {'User-Agent': "GitHub Avatar Downloader - Student Project" } };
+
+ request(option, function (err, result, body){
+   //cb e' il callback e si riferisce all'ultima funzione e facciao il parse con il json
+   //perche' vogliamo il formato in quel modo e ci riferiamo al body perche' e' il corpo
+  // del nostro documento
+   cb(err,JSON.parse(body))
+ console.log(body);
+});
+}
+
+getRepoContributors("jquery", "jquery", function(err,body) {
+ var avatarArray = [];
+ body.forEach(function(name){
+   avatarArray.push(body.avatar_url)
+ })
+ console.log("Errors:", err);
+ console.log("Result:", avatarArray);
+});
+*/
